50b0756ed0db1b8084b889d0422d93bda16ccad4
firstwebsite <joyroy04010@gmail.com>
2021-01-22 10:20:09 +0600

    first commit

---
+++ kilometerToMeter.js
@@ -0,0 +1,11 @@
+function kilometerToMeter(kilometer){
+    var meter = kilometer * 1000;
+    return meter;
+}
+// var firstOneMeter = kilometerToMeter(123);
+// console.log(rahim);
+
+var rahim = [12, 34, 45, 60];
+
+var nanaMeter = kilometerToMeter(rahim[1]);
+console.log(nanaMeter);
---
+++ loveApp.js
@@ -0,0 +1,12 @@
+// person name
+
+let firstName = prompt("What is your name? ");
+let partnerName = prompt("What is your partners name?");
+
+// calculation
+let number = Math.random() * 100;
+number = Math.floor(number)*1;
+
+// Showing result
+
+alert(firstName + " and " + partnerName + "'S Love percentage is : " + number + "%")
